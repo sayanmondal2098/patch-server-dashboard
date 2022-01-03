29159c9e6e44c27a1fed4da8db29ebf0dcc13fec
Sayan Mondal <sayanmondal2098@gmail.com>
2022-01-03 20:44:59 +0530

    basic routing added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import "./App.css";
+import UserList from "./pages/userList/userList";
 
 
 function App() {
   return (
-    <div className="App">
-      <Topbar/>
-      <div className="container">
-        <Sidebar/>
-        <Home/>
+    <Router>
+      <div className="App">
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/userlist" element={<UserList />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/userList/userList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./userList.css";
+
+
+export default function UserList() {
+    return (
+        <div className="userList"> 
+            <h1>User List</h1>
+        </div> 
+    );
+}
