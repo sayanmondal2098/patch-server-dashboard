035c9cba77dab8134402547c59af7db1f67d8a78
Sayan Mondal <sayanmondal2098@gmail.com>
2022-01-04 14:23:44 +0530

    update basic delete

--- src/pages/userList/userList.jsx
+++ src/pages/userList/userList.jsx
@@ -1,49 +1,71 @@
 import React from 'react';
 import { DataGrid } from '@material-ui/data-grid'
+import { DeleteOutlineTwoTone } from '@material-ui/icons'
+import { userRows } from "../../dummyData";
 import "./userList.css";
+import { Link } from 'react-router-dom';
 
 
 export default function UserList() {
+    const [rows, setRows] = React.useState(userRows);
+    const hendleDelete = (id) => {  
+        setRows(rows.filter(row => row.id !== id));
+    };
+    
     const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'username', headerName: 'username', width: 200 },
+        { field: 'email', headerName: 'User Email', width: 330 },
         {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 90,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
+            field: 'status',
+            headerName: 'Status',
             width: 160,
-            valueGetter: (params) =>
-                `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
-                }`,
         },
+        {
+            field: 'jobRoll',
+            headerName: 'Job Roll',
+            description: 'This column has a value getter and is not sortable.',
+            sortable: true,
+            width: 160,
+
+        },
+        {
+            field: 'StartDate',
+            headerName: 'Start Date',
+            description: 'This column has a value getter and is not sortable.',
+            type: 'date',
+            sortable: true,
+            width: 160,
+
+        },
+        {
+            field: 'Action',
+            headerName: 'Action',
+            width: 160,
+            renderCell: (params) => {
+                
+                return (
+                <>
+                <Link to={"/user/"+params.row.id}>
+                <button className="userListEdit">Edit</button>
+                </Link>
+                <DeleteOutlineTwoTone className="userListDelete" 
+                onClick={() => hendleDelete(params.row.id)}
+                />
+                </>
+                );
+            }
+        }
     ];
 
-    const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
     return (
         <div className="userList">
             <DataGrid
                 rows={rows}
+                disableSelectionOnClick
                 columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
+                pageSize={8}
+                rowsPerPageOptions={[100]}
                 checkboxSelection
             />
         </div>
